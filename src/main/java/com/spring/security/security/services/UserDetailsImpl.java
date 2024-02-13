feab7b2ce9d4569401aa239d72a94324f28da325
Tommy Dahlin <tommy.dahlin@studerande.plushogskolan.se>
2024-02-13 10:44:47 +0100

    implement Userdetailservice with userrepository, and added loadUserByName

--- src/main/java/com/spring/security/security/services/UserDetailsImpl.java
+++ src/main/java/com/spring/security/security/services/UserDetailsImpl.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class UserDetailsImpl implements UserDetails {
@@ -38,5 +39,49 @@ public class UserDetailsImpl implements UserDetails {
                 user.getPassword(),
                 authorities);
         )
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+    @Override
+    public boolean equals(Object o){
+        if(this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        UserDetailsImpl user = (UserDetailsImpl) o;
+        return Objects.equals(id, user.id);
     }
 }
--- src/main/java/com/spring/security/security/services/UserDetailsServiceImpl.java
+++ src/main/java/com/spring/security/security/services/UserDetailsServiceImpl.java
@@ -1,4 +1,25 @@
 package com.spring.security.security.services;
 
-public class UserDetailsServiceImpl {
+import com.spring.security.models.User;
+import com.spring.security.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException("User was not found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
 }
