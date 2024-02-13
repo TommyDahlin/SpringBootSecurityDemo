85af3f34730f97ada6d9b8149d9b6399a72ce09c
Tommy Dahlin <tommy.dahlin@studerande.plushogskolan.se>
2024-02-13 09:22:22 +0100

    first push

--- src/main/java/com/spring/security/models/User.java
+++ src/main/java/com/spring/security/models/User.java
@@ -1,7 +1,16 @@
 package com.spring.security.models;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
 public class User {
+    @Id
+    private String id;
+    @NotBlank
+    @Size(max = 50)
+    private String username;
+
 }
